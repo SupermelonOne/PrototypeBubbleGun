12303d83c63d948ef380eca491a4e489f0fd6d00
Vezko <549065@student.saxion.nl>
2025-06-03 14:46:10 +0200

    Fix disabled keyboard
    fixing the issue where players couldn't join using the keyboard and mouse controls.

--- Assets/InputManager/PlayerInputActions.cs
+++ Assets/InputManager/PlayerInputActions.cs
@@ -340,11 +340,6 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
                     ""devicePath"": ""<Mouse>"",
                     ""isOptional"": false,
                     ""isOR"": false
-                },
-                {
-                    ""devicePath"": ""<Gamepad>"",
-                    ""isOptional"": false,
-                    ""isOR"": false
                 }
             ]
         },
