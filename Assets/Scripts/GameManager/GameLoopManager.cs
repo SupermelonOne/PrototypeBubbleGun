b8d2984e5efa8fb5cb581a4b62c304f48dc824b2
Joris <wassinkjoris@gmail.com>
2025-07-02 13:56:41 +0200

    GameLoop done

--- Assets/Scripts/GameManager/GameLoopManager.cs
+++ Assets/Scripts/GameManager/GameLoopManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameLoopManager : MonoBehaviour
 {
@@ -10,7 +11,12 @@ public class GameLoopManager : MonoBehaviour
     [SerializeField] private int seconds;
     [SerializeField] private int highScoreAmount;
 
+    private int initialMinutes;
+    private int initialSeconds;
+
     //c# events so everything can subscribe without editor bs
+    public bool menuMode = true;
+    public bool dead = false;
     public event Action OnGameOver;
     public event Action<int, int> OnSecondPassed;
     
@@ -29,10 +35,15 @@ public class GameLoopManager : MonoBehaviour
             return;
         }
         Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
     private void Start()
     {
+        // save initial values
+        initialMinutes = minutes;
+        initialSeconds = seconds;
+
         //creating the file
         path = Path.Combine(Application.persistentDataPath, "highscores.txt");
         if (!File.Exists(path))
@@ -41,6 +52,12 @@ public class GameLoopManager : MonoBehaviour
 
     void Update()
     {
+        if(Input.anyKeyDown && (menuMode || dead))
+            SwitchScene();
+        
+        if (menuMode || dead)
+            return;
+
         timer += Time.deltaTime;
 
         if (!(timer >= 1f)) return;
@@ -58,6 +75,7 @@ public class GameLoopManager : MonoBehaviour
         {
             AddHighScore(PlayerInventory.Instance.ItemAmount(ItemType.Munny));
             OnGameOver?.Invoke();
+            SwitchScene();
             return;
         }
             
@@ -66,7 +84,34 @@ public class GameLoopManager : MonoBehaviour
         timer = 0f;
     }
 
-    private int[] GetHighScores()
+    private void SwitchScene()
+    {
+        if (SceneManager.GetActiveScene().buildIndex + 1 >= SceneManager.sceneCountInBuildSettings)
+            SceneManager.LoadScene(0);
+        else
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        if (dead)
+        {
+            menuMode = true;
+            dead = false;
+            timer = 0f;
+            minutes = initialMinutes;
+            seconds = initialSeconds;
+        }
+        else if (menuMode)
+        {
+            menuMode = false;
+            dead = false;
+        }
+        else
+        {
+            menuMode = false;
+            dead = true;
+        }
+    }
+
+    public int[] GetHighScores()
     {
         highScores = new int [highScoreAmount];
         try
@@ -91,19 +136,32 @@ public class GameLoopManager : MonoBehaviour
         var scoreList = highScores.ToList();
         scoreList.Add(score);
         scoreList.Sort();
-        scoreList.RemoveAt(scoreList.Count - 1);
+        scoreList.RemoveAt(0);
+        scoreList.Reverse();
         highScores = scoreList.ToArray();
+        for (var i = 0; i < highScores.Length; i++)
+        {
+            Debug.Log(highScores[i]);
+        }
         SaveHighScores();
     }
 
     private void SaveHighScores()
     {
-        StreamWriter sw = new StreamWriter(path);
-        
-        for (int i = 0; i < highScoreAmount; i++)
+        try
         {
-            var s = highScores[i];
-            sw.WriteLine(s);
+            using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.UTF8))
+            {
+                for (int i = 0; i < highScoreAmount; i++)
+                {
+                    var s = (highScores != null && i < highScores.Length) ? highScores[i] : 0;
+                    sw.WriteLine(s);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Failed to save high scores: " + e.Message);
         }
     }
 }
